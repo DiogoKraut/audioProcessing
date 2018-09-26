40e5641b204ca102050c7891b57e140b22170e64
DiogoKraut <diogo.kraut@hotmail.com>
2018-09-26 00:39:01 -0300

    TAD changes, access functions added and started working on Rev effect

---
+++ wavTAD.h
@@ -0,0 +1,22 @@
+#include <inttypes.h>
+
+typedef struct sHEADER {
+	char chunkID[4];
+	uint32_t chunkSize;
+	char     format[4];
+	char     subChunk1ID[4];
+	uint32_t subChunk1Size;
+	uint16_t audio_format;
+	uint16_t num_channels;
+	uint32_t sample_rate;
+	uint32_t byte_rate;
+	uint16_t block_align;
+	uint16_t bits_per_sample;
+	char     subChunk2ID[4];
+	uint32_t subChunk2Size;
+} tHeader;
+
+typedef struct sWAV {
+	tHeader *header;
+	int *data;
+}
